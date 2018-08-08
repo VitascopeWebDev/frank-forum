dbd1b9257b39dabf9f6958e085e3c47b05d32757
Utilisateur-PC\FrancisCampeau <frank.camps19@gmail.com>
2018-08-08 13:49:52 -0400

    initial commit

---
+++ client/src/components/AppNav.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Frank's project</h1>
+      <p class="lead">This is a simple hero unit for the frank's community project</p>
+      <hr class="my-4">
+      <p>This is a tryout forum to practice simple full stack Web development app</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home'
+};
+</script>
